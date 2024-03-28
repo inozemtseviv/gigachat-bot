5d8a98d5078eb9357c8a26dfe0c7851155144ab8
Igor Inozemtsev <inozemtsev.iv@gmail.com>
2024-03-28 18:39:52 +0300

    Код бота вынесен в отдельный модуль

---
+++ app/bot/bot.py
@@ -0,0 +1,28 @@
+from telebot import types, TeleBot
+
+
+class Bot:
+    def __init__(self, token: str):
+        self.bot = TeleBot(token)
+        self.setup_handlers()
+
+    def setup_handlers(self):
+        @self.bot.message_handler(commands=['start'])
+        def start_bot(message):
+            first_mess = f"<b>{message.from_user.first_name} {message.from_user.last_name}</b>, привет!\nХочешь что-то узнать?"
+            markup = types.InlineKeyboardMarkup()
+            button_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
+            markup.add(button_yes)
+            self.bot.send_message(message.chat.id, first_mess, parse_mode='html', reply_markup=markup)
+
+        @self.bot.callback_query_handler(func=lambda call: True)
+        def response(function_call):
+            if function_call.message:
+                if function_call.data == 'yes':
+                    second_mess = 'Задай свой вопрос и я поищу для тебя ответ.'
+                    markup = types.InlineKeyboardMarkup()
+                    self.bot.send_message(function_call.message.chat.id, second_mess, reply_markup=markup)
+                    self.bot.answer_callback_query(function_call.id)
+
+    def run(self):
+        self.bot.infinity_polling()
--- main.py
+++ main.py
@@ -1,29 +1,10 @@
 import os
-from telebot import types, TeleBot
 from dotenv import load_dotenv
+
+from app.bot.bot import Bot
 
 load_dotenv()
 
-tg_giga = TeleBot(os.getenv('TG_TOKEN'))
+tg_giga = Bot(os.getenv('TG_TOKEN'))
 
-
-@tg_giga.message_handler(commands=['start'])
-def start_bot(message):
-    first_mess = f"<b>{message.from_user.first_name} {message.from_user.last_name}</b>, привет!\nХочешь что-то узнать?"
-    markup = types.InlineKeyboardMarkup()
-    button_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
-    markup.add(button_yes)
-    tg_giga.send_message(message.chat.id, first_mess, parse_mode='html', reply_markup=markup)
-
-
-@tg_giga.callback_query_handler(func=lambda call: True)
-def response(function_call):
-    if function_call.message:
-        if function_call.data == 'yes':
-            second_mess = 'Задай свой вопрос и я поищу для тебя ответ.'
-            markup = types.InlineKeyboardMarkup()
-            tg_giga.send_message(function_call.message.chat.id, second_mess, reply_markup=markup)
-            tg_giga.answer_callback_query(function_call.id)
-
-
-tg_giga.infinity_polling()
+tg_giga.run()
